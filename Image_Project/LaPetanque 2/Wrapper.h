0c55bf1e0db8c4a254626833f6b69cb1d623a6a4
RaphaelBrdMss <78405712+RaphaelBrdMss@users.noreply.github.com>
2021-12-07 13:25:57 +0100

    minor code update

---
+++ Image_Project/LaPetanque 2/Wrapper.h
@@ -0,0 +1,22 @@
+//
+//  Wrapper.h
+//  LaPetanque
+//
+//  Created by Raphael Braud-Mussi on 08/11/2021.
+//
+
+#ifndef Wrapper_h
+#define Wrapper_h
+#import <UIKit/UIKit.h>
+#include <Foundation/Foundation.h>
+
+
+
+@interface Wrapper: NSObject
+
+
++(UIImage *) findCircle : (UIImage *) image;
+
+@end
+
+#endif /* Wrapper_h */
