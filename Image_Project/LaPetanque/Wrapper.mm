c6bbed3292d3c9a6cea78abcfc98b1c7bfb8ec1c
RaphaelBrdMss <78405712+RaphaelBrdMss@users.noreply.github.com>
2021-12-14 16:16:31 +0100

    Commentaire dans la class C++ (traitement d'image) et ajout de documents additionnels

--- Image_Project/LaPetanque/Wrapper.mm
+++ Image_Project/LaPetanque/Wrapper.mm
@@ -1,4 +1,4 @@
-//
+
 //  Wrapper.m
 //  LaPetanque
 //
@@ -20,7 +20,7 @@ using namespace std;
 
 @implementation Wrapper
 
-
+//structure deffinissant une boule
 struct boule{
     double distCochonnet;
     int radius;
@@ -31,12 +31,13 @@ struct boule{
 };
 
 
+//algorithme de tri
 int partitionnage(vector<boule>& temp, int deb, int end){
     int cpt = deb;
     double pivot = temp[deb].distCochonnet;
     int i;
     for(  i =deb+1 ; i<end; i++){
-        if(temp[i].distCochonnet<=pivot){
+        if(temp[i].distCochonnet<pivot){
             cpt++;
             iter_swap(temp.begin()+cpt, temp.begin()+i);
             
@@ -73,29 +74,43 @@ void tri_rapide(vector<boule>& boules){
     
    
     Mat grayimg;
-    //GaussianBlur(imageMat, imageMat, cv::Size(5,5), 1.5 ,1.5, BORDER_DEFAULT);
-    medianBlur(imageMat, grayimg, 7);
+    
+    medianBlur(imageMat, grayimg, 15);
     cvtColor(grayimg, grayimg, COLOR_BGR2GRAY);
     rotate(grayimg , grayimg, cv::ROTATE_90_CLOCKWISE);
+    // boucle qui lisse les pixels trop blanc et reduit les rend plus gris
+    for(int i=0; i<grayimg.rows; i++){
+        for(int j=0; j<grayimg.cols; j++){
+            if(grayimg.at<uchar>(i,j) >200 ){
+                grayimg.at<uchar>(i,j) = 150;
+            }
+        }
+    }
+        
     vector<Vec3f> circles;
+        //transformé de hough qui permet de detecter les cercle dans l'image
+    //parm1 : grayimg, image sur la quelle la transformé va s'appliquer
+    //parm2 : circles, liste de Vec3 liste de cercle
+    //parm3 :  HOUGH_GRADIENT_ALT, type de transformé utilisant le filtre de Scharr
+    //param4 : 1, echelle de l'image (1 = inchangé)
+    //param5 : 200, seuil max pour la detection de canny (Min = Max/2)
+    //param6 : 0.8,  taux de perfection d'un cercle (1= cercle parfait)
+    //param7 : 15, rayon minimum d'un cercle
+    //param8 : 60, rayon maximym d'un cercle
+       HoughCircles(grayimg, circles, HOUGH_GRADIENT_ALT, 1,
+                    100,
+                    200, 0.80, 15, 60);
     
-       HoughCircles(grayimg, circles, HOUGH_GRADIENT, 1,
-                    100,  // change this value to detect circles with different distances to each other
-                    100, 30, 10, 50 // change the last two parameters
-               // (min_radius & max_radius) to detect larger circles
-                    
-        );
+    
     
     Vec3f cochonet;
     int minRadius=500;
-    
     vector<boule> boules;
-    
+    //recherche du plus petit cercle (cochonet)
     for( size_t i = 0; i < circles.size(); i++ )
      {
          
          if(minRadius>circles[i][2]){
-             printf("hola radius\n");
              minRadius = circles[i][2];
              cochonet = circles[i];
              
@@ -112,7 +127,7 @@ void tri_rapide(vector<boule>& boules){
     boules.push_back(cochon);
     
     
-    
+    //calcule des distance exacte entre les boules et le cochonet
     for( size_t i = 0; i < circles.size(); i++ ){
         
         if(circles[i][2] == cochon.radius){
@@ -122,7 +137,7 @@ void tri_rapide(vector<boule>& boules){
         {
         boule Currentboule;
             Currentboule.distCochonnet = cv::norm(cv::Point(cochonet[0],cochonet[1])-cv::Point(circles[i][0],circles[i][1]))
-            -cochonet[2]-circles[i][2]; //distance du bord de la boule avec le bord du cochonet. pour prendre en comtpe la largeur des boules. 
+            -cochonet[2]-circles[i][2]; //distance du bord de la boule avec le bord du cochonet. pour prendre en comtpe la largeur des boules.
             
         Currentboule.centre = cv::Point(circles[i][0],circles[i][1]);
             
@@ -133,45 +148,46 @@ void tri_rapide(vector<boule>& boules){
     }
     
     rotate(imageMat, imageMat, cv::ROTATE_90_CLOCKWISE);
+    //tri des boule en fonction de leur distance.
     tri_rapide(boules);
     int cpt =1;
+    
+    //affichage des boules sur l'image et suppresion des eventuels cercle fantome.
     for(boule b : boules){
         
         
         if(!b.isCochonet && b.distCochonnet>0){
             std::cout << "nb : " << cpt << "\n";
             std::cout << b.distCochonnet << "\n";
+            //std::cout << b.radius << "\n";
+           
+            if(b.radius<20|| b.radius>40){
             
-            if( b.radius < 20 || b.radius> 40){
-            cv::putText(imageMat, to_string(cpt), cv::Point(b.centre.x+30,b.centre.y+30), FONT_HERSHEY_PLAIN, 5.0, Scalar(250,0,50,255),2.0);
+            cv::putText(imageMat, to_string(cpt), cv::Point(b.centre.x+70,b.centre.y+70), FONT_HERSHEY_PLAIN, 5.0, Scalar(250,0,50,255),2.0);
             cv::circle( imageMat, b.centre, b.radius, Scalar(0,162,255,255), 4, LINE_AA);
             cv::circle( imageMat, b.centre, 1, Scalar(0,100,100,255), 8, LINE_AA);
             cv::line(imageMat, cv::Point(cochonet[0],cochonet[1]), cv::Point(b.centre), Scalar(0,0,0,255),3,LINE_AA);
             cpt++;
-            }
-            else { continue; }
+            }else{continue;}
+          
+            
+            
                
         }
         else
         {
-            printf("cochon \n");
+            std::cout << b.radius << "\n";
             cv::circle( imageMat, b.centre, b.radius, Scalar(0,255,0,255), 3, LINE_AA);
             cv::circle( imageMat, b.centre, 1, Scalar(0,100,100,255), 6, LINE_AA);
                      
            
         }
-        
-        
-        
+    
         
     }
 
     
-    
-    
-    
-    
-    UIImage * processedImg = MatToUIImage(imageMat);
+    UIImage * processedImg = MatToUIImage(grayimg);
     
     
     return processedImg ;
